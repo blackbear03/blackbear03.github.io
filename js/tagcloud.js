fc7970bc968626173910221f07ac0ddae70083cc
mark <blackbear03.w@gmail.com>
2024-05-01 22:47:36 +0800

    Happy writing :)

--- js/tagcloud.js
+++ js/tagcloud.js
@@ -15,7 +15,7 @@
 
      try {
          TagCanvas.textFont = 'Trebuchet MS, Helvetica';
-         TagCanvas.textColour = '#adb5bd0f';
+         TagCanvas.textColour = '#ffffff1a';
          TagCanvas.textHeight = 22;
          TagCanvas.outlineColour = '#2a6496';
          TagCanvas.maxSpeed = 0.1;
